948924122ab4e1f04754239b91c7bf15f9936c09
Edwin <edwinthomasg29@gmail.com>
2022-04-27 18:06:30 +0530

    commit

---
+++ Console/console.js
@@ -0,0 +1,13 @@
+console.error("error ocurred")
+console.log("Group start")
+console.group()
+console.log("1")
+console.log("2")
+console.log("2")
+console.log("2")
+console.log("2")
+console.groupEnd()
+console.log("Group end")
+console.log(new Error("err"))
+console.trace()
+console.assert("ed"=="edi","Fails") /**return message only when it fails */
