a25c0db0b5a4a33af843528f90bdfaff229c6f7d
edwinthomasg <edwinthomasg29@gmail.com>
2022-12-13 16:29:16 +0530

    change

---
+++ file.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pwd #no error returns current working dir
+find / #maximum buffer exceeded error so go for spawn
--- test.js
+++ test.js
@@ -1,7 +1,71 @@
-let ar = [1,2,3]
-val = 20
-ar.forEach((val)=>{
-    console.log(val)
-})
-for(let val of ar)
-console.log(val)
+// const { execFile } = require("child_process")
+
+// // let ar = [1,2,3]
+// // val = 20
+// // ar.forEach((val)=>{
+// //     console.log(val)
+// // })
+// // for(let val of ar)
+// // console.log(val)
+
+// // execFile("./file.sh", (err, stdout, stderr) => {
+// //     if(err)
+// //     console.log("Command Error : ",err)
+// //     if(stderr)
+// //     console.log("File error : ",stderr)
+// //     console.log("output : ",stdout)
+// // })
+
+// const iterate = (start = 0, end = Infinity) => {
+//     let index = 0
+//     let nextIndex = start
+//     const iterator = {
+//         next(){
+//             console.log("start : ",start)
+//             console.log("nextIndex : ",nextIndex)
+//             let result
+//             if(nextIndex < 10)
+//                 {
+//                     result = { value: nextIndex, done: false}
+//                     index++
+//                     nextIndex++
+//                     return result
+//                 }
+//             return { value: index, done: true }
+//         }
+//     }
+//     return iterator
+// }
+
+// let itr = iterate(1,10)
+// let res = itr.next()
+// while(!res.done)
+// {
+//     console.log(res)
+//     res = itr.next()
+// }
+
+function* gen(start, end){
+    for(var i = 0; i < end; i++)
+    {
+        yield i
+    }
+    return i
+}
+
+// let generator = gen(1,5)
+// let res = generator.next()
+
+// while(!res.done)
+// {
+//     console.log(res)
+//     res = generator.next()
+// }
+
+function* sample(){
+    yield 1
+    yield 2
+}
+let res = sample()
+console.log(res.next())
+console.log(res.next())
